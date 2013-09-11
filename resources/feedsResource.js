c351ad438f11af4e202e21b447005778a007e19b
csauve <connorsauve@gmail.com>
2013-09-11 15:45:07 -0700

    Implemented partial update

--- resources/feedsResource.js
+++ resources/feedsResource.js
@@ -45,7 +45,15 @@ module.exports = {
 
     update: function(req, res) {
         Feed.findById(req.params.id, function(error, result) {
-            console.log(result);
+            if (error) {
+                console.log(error);
+                return res.send(400);
+            }
+            if (!result) {
+                return res.send(404);
+            }
+            copyFields(Feed, req.body, result);
+            result.save();
             res.send(result);
         });
     },
