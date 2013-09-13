f346f1e5d82c8b935e7fb3e707de183eb5f0a3df
csauve <connorsauve@gmail.com>
2013-09-13 08:09:09 -0700

    Completed Not Found and About Us pages

--- web-app/static/js/app.js
+++ web-app/static/js/app.js
@@ -1,5 +1,10 @@
 App = Ember.Application.create();
 
-// App.Router.map(function() {
-    
-// });
+App.Router.reopen({
+	location: "history"
+});
+
+App.Router.map(function() {
+    this.resource("about");
+    this.route("notFound", {path: "/*path"});
+});
