2df50868b963cedd94ea660f1a698153251eff27
csauve <connorsauve@gmail.com>
2013-09-18 15:10:30 -0700

    Switching from ember to angular

--- web-app/static/js/app.js
+++ web-app/static/js/app.js
@@ -1,13 +1,28 @@
-angular.module("traveler", []).config(function($routeProvider, $locationProvider) {
+var app = angular.module("traveler", []).config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-    .when("/", {controller: HomeCtrl, templateUrl: "/static/html/home.html"})
+    //.when("/", {controller: HomeCtrl, templateUrl: "/static/html/home.html"})
+    .when("/", {templateUrl: "/static/html/home.html"})
     .when("/about", {templateUrl: "/static/html/about.html"})
-    .when("/links", {controller: "LinksCtrl", templateUrl: "/static/html/links.html"})
-    .when("/roundup", {controller: "RoundupCtrl", templateUrl: "/static/html/roundup.html"})
-    .when("/archive", {controller: "ArchiveCtrl", templateUrl: "/static/html/archive.html"})
-    .when("/tags", {controller: "TagsCtrl", templateUrl: "/static/html/tags.html"})
-    .when("/subscribe", {controller: "SubscribeCtrl", templateUrl: "/static/html/subscribe.html"})
-    .when("submit", controller: "SubmitCtrl", templateUrl: "/static/html/submit.html")
-    .when("/feeds", {controller: "FeedsCtrl", templateUrl: "/static/html/feeds.html"});
+    .when("/links", {templateUrl: "/static/html/links.html"})
+    // .when("/roundup", {controller: "RoundupCtrl", templateUrl: "/static/html/roundup.html"})
+    // .when("/archive", {controller: "ArchiveCtrl", templateUrl: "/static/html/archive.html"})
+    // .when("/tags", {controller: "TagsCtrl", templateUrl: "/static/html/tags.html"})
+    // .when("/subscribe", {controller: "SubscribeCtrl", templateUrl: "/static/html/subscribe.html"})
+    // .when("submit", controller: "SubmitCtrl", templateUrl: "/static/html/submit.html")
+    // .when("/feeds", {controller: "FeedsCtrl", templateUrl: "/static/html/feeds.html"})
+    .otherwise({controller: "NotFoundCtrl", templateUrl: "/static/html/notfound.html"});
 });
+
+app.run(function($rootScope, $location) {
+    $rootScope.location = $location;
+});
+
+function NotFoundCtrl($scope) {
+    //todo: use fuzzy search on slugs with url
+    $.get("/api/posts", function(response) {
+        $scope.$apply(function() {
+            $scope.results = [];
+        });
+    });
+}
