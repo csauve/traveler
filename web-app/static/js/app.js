241093335ff61630abd84a1601f17672a220b857
csauve <connorsauve@gmail.com>
2013-09-18 17:22:47 -0700

    Added feeds page

--- web-app/static/js/app.js
+++ web-app/static/js/app.js
@@ -10,13 +10,21 @@ var app = angular.module("traveler", []).config(function($routeProvider, $locati
     // .when("/tags", {controller: "TagsCtrl", templateUrl: "/static/html/tags.html"})
     // .when("/subscribe", {controller: "SubscribeCtrl", templateUrl: "/static/html/subscribe.html"})
     // .when("submit", controller: "SubmitCtrl", templateUrl: "/static/html/submit.html")
-    // .when("/feeds", {controller: "FeedsCtrl", templateUrl: "/static/html/feeds.html"})
+    .when("/feeds", {controller: "FeedsCtrl", templateUrl: "/static/html/feeds.html"})
     .otherwise({controller: "NotFoundCtrl", templateUrl: "/static/html/notfound.html"});
 });
 
 app.run(function($rootScope, $location) {
     $rootScope.location = $location;
 });
+
+function FeedsCtrl($scope) {
+    $.get("/api/feeds", function(response) {
+        $scope.$apply(function() {
+            $scope.feeds = response;
+        });
+    });
+}
 
 function NotFoundCtrl($scope) {
     //todo: use fuzzy search on slugs with url
